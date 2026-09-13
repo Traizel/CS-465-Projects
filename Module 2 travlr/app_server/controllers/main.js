41ec0689d4ebe70e2e327a8c9688dc0343da78b4
Traizel <treolson@live.com>
2026-09-13 15:20:50 -0500

    remove module 2 on branch module1

--- Module 2 travlr/app_server/controllers/main.js
+++
@@ -1,6 +0,0 @@
-// Render the index page through the Handlebars view engine.
-const index = (req, res) => {
-  res.render('index', { title: 'Travlr Getaways', isHome: true });
-};
-
-module.exports = { index };
--- Module 2 travlr/app_server/controllers/travel.js
+++
@@ -1,6 +0,0 @@
-// Render the travel page through the Handlebars view engine.
-const travel = (req, res) => {
-  res.render('travel', { title: 'Travlr Getaways', isTravel: true });
-};
-
-module.exports = { travel };
